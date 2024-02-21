a1a708c1887694332cc6c34df7817f1b68c4d583
awais-shahid-nextech <awaisshahid@rapidzzsolutions.com>
2024-02-21 20:41:39 +0500

    feat: added pre-commit hooks and lint-staged

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const expressApp = express()
 
 expressApp.get('/', (_, res) => {
-    res.json({okSir: true})
+    res.json({ okSir: true })
 })
 
 expressApp.listen(3000, () => {
