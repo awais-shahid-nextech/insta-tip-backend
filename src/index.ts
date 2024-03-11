ac6bff5d1dd519bb88bf45e1f839392dfaadc27a
Jawad Khan <jawadkhan918@gmail.com>
2024-03-11 12:18:01 +0500

    feat: added mongodb and odm mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import http from 'http'
 import { expressApp } from './config/app'
+import { initializeMongoDB } from './config/mongodb'
 
 const PORT = process.env.PORT ?? 8000
 
 const server = http.createServer(expressApp)
 
 async function startServer() {
-    server.listen(PORT, () => {
-        console.log(`Server listening on port: ${PORT}`)
-    })
+    try {
+        await initializeMongoDB()
+        server.listen(PORT, () => {
+            console.log(`Server listening on port: ${PORT}`)
+        })
+    } catch (error) {
+        console.error((error as { message: string }).message)
+        process.exit(1)
+    }
 }
 
 startServer()
